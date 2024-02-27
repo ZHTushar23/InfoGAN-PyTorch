ce5603c35217b5e1182f552108017eb2b6f87d46
zhtushar23 <zahidhassantushar@gmail.com>
2024-02-26 21:50:14 -0500

    updates

--- v1_cloud_generate.py
+++ v1_cloud_generate.py
@@ -27,7 +27,17 @@ netG = Generator().to(device)
 netG.load_state_dict(state_dict['netG'])
 print(netG)
 
-noise1 , _ = noise_sample(10,10,0,128,9,device)
+# noise1 , _ = noise_sample(10,10,0,128,9,device)
+
+idx = np.arange(3).repeat(3)
+dis_c = torch.zeros(9, 10, 10, device=device)
+dis_c[torch.arange(0, 9), idx] = 1.0
+# Discrete latent code.
+c1 = dis_c.view(9, -1, 1, 1)
+z = torch.randn(9, 128, 1, 1, device=device)
+noise1 = torch.cat((z, c1), dim=1)
+
+
 
 # Generate image.
 with torch.no_grad():
@@ -35,3 +45,10 @@ with torch.no_grad():
 generated_img1 = generated_img1.numpy()
 fname = "v1_cloud/"+"gen_img.png"
 plot_cot2(generated_img1[0,0],"Radiance at 0.66um",fname,False,[0,2])
+
+# b = dis_c.detach().cpu().numpy()
+# print(b.shape)
+# plt.imshow(b[5])
+# plt.colorbar()
+# plt.savefig('demo.png')
+# plt.close()
