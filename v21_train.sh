38a3f01b8a143e97af824ff5abc42e09819401ed
zhtushar23 <zahidhassantushar@gmail.com>
2024-03-04 19:44:59 -0500

    updates

---
+++ v21_train.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+device=$1
+python v21_train_cloud.py --device $device
---
+++ v21_train_cloud.py
@@ -0,0 +1,345 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
+import random
+import argparse
+
+from models.mnist_model import Generator, Discriminator, DHead, QHead
+# from dataloader import get_data
+from v2_utils import *
+from v21_config import params
+from v21_dataset import NasaDataset
+from visualization import *
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train a model')
+    parser.add_argument('--device', type=int,  default=1, help='CUDA')
+    args = parser.parse_args()
+    return args
+
+
+if(params['dataset'] == 'MNIST'):
+    from models.mnist_model import Generator, Discriminator, DHead, QHead
+elif(params['dataset'] == 'SVHN'):
+    from models.svhn_model import Generator, Discriminator, DHead, QHead
+elif(params['dataset'] == 'CelebA'):
+    from models.celeba_model import Generator, Discriminator, DHead, QHead
+elif(params['dataset'] == 'FashionMNIST'):
+    from models.mnist_model import Generator, Discriminator, DHead, QHead
+elif(params['dataset'] == 'Cloud'):
+    # Need to decide new models
+    # Consider the input, output size of each blocks
+    from models.cloud_model import Generator, Discriminator, DHead, QHead
+
+elif(params['dataset'] == 'Cloud2'):
+    # Need to decide new models
+    # Consider the input, output size of each blocks
+    from models.cloud_model2 import Generator, Discriminator, DHead, QHead
+
+elif(params['dataset'] == 'Cloud3'):
+    # Need to decide new models
+    # Consider the input, output size of each blocks
+    from models.cloud_model3 import Generator, Discriminator, DHead, QHead
+
+elif(params['dataset'] == 'Cloud21'):
+    # Need to decide new models
+    # Consider the input, output size of each blocks
+    from models.cloud_model21 import Generator, Discriminator, DHead, QHead
+
+# Set random seed for reproducibility.
+seed = 1123
+random.seed(seed)
+torch.manual_seed(seed)
+print("Random Seed: ", seed)
+
+# Use GPU if available.
+# device = torch.device("cuda:0" if(torch.cuda.is_available()) else "cpu")
+# Parse the arguments
+args = parse_args()
+
+# Check if the GPU is available
+device = torch.device(args.device) if torch.cuda.is_available() else torch.device("cpu")
+
+print(device, " will be used.\n")
+
+# dataloader = get_data(params['dataset'], params['batch_size'])
+dataset_dir ="/home/local/AD/ztushar1/LES102_MultiView_100m_F2/"
+dataset = NasaDataset(root_dir=dataset_dir,mode="train")
+dataloader = torch.utils.data.DataLoader(dataset, 
+                                            batch_size=params['batch_size'], 
+                                            shuffle=True)
+
+# Set appropriate hyperparameters depending on the dataset used.
+# The values given in the InfoGAN paper are used.
+# num_z : dimension of incompressible noise.
+# num_dis_c : number of discrete latent code used.
+# dis_c_dim : dimension of discrete latent code.
+# num_con_c : number of continuous latent code used.
+if(params['dataset'] == 'MNIST'):
+    params['num_z'] = 62
+    params['num_dis_c'] = 1
+    params['dis_c_dim'] = 10
+    params['num_con_c'] = 2
+elif(params['dataset'] == 'SVHN'):
+    params['num_z'] = 124
+    params['num_dis_c'] = 4
+    params['dis_c_dim'] = 10
+    params['num_con_c'] = 4
+elif(params['dataset'] == 'CelebA'):
+    params['num_z'] = 128
+    params['num_dis_c'] = 10
+    params['dis_c_dim'] = 10
+    params['num_con_c'] = 0
+elif(params['dataset'] == 'FashionMNIST'):
+    params['num_z'] = 62
+    params['num_dis_c'] = 1
+    params['dis_c_dim'] = 10
+    params['num_con_c'] = 2
+
+elif(params['dataset'] == 'Cloud'):
+    params['num_z'] = 128
+    params['num_dis_c'] = 10
+    params['dis_c_dim'] = 10
+    params['num_con_c'] = 0
+
+elif(params['dataset'] == 'Cloud2'):
+    params['num_z'] = 1
+    params['num_dis_c'] = 11
+    params['dis_c_dim'] = 72
+    params['num_con_c'] = 0
+
+elif(params['dataset'] == 'Cloud3'):
+    params['num_z'] = 1
+    params['num_dis_c'] = 11
+    params['dis_c_dim'] = 72
+    params['num_con_c'] = 0
+
+elif(params['dataset'] == 'Cloud21'):
+    params['num_z'] = 1
+    params['num_dis_c'] = 11
+    params['dis_c_dim'] = 72
+    params['num_con_c'] = 0
+# # Plot the training images.
+# sample_batch = next(iter(dataloader))
+# plt.figure(figsize=(3, 3))
+# plt.axis("off")
+# plt.imshow(np.transpose(vutils.make_grid(
+#     sample_batch['reflectance'][0].to(device)[ : 9], nrow=3, padding=2, normalize=True).cpu(), (1, 2, 0)))
+# plt.savefig('Training Images {}'.format(params['dataset']))
+# plt.close('all')
+
+# Initialise the network.
+netG = Generator().to(device)
+netG.apply(weights_init)
+print(netG)
+
+discriminator = Discriminator().to(device)
+discriminator.apply(weights_init)
+print(discriminator)
+
+netD = DHead().to(device)
+netD.apply(weights_init)
+print(netD)
+
+netQ = QHead().to(device)
+netQ.apply(weights_init)
+print(netQ)
+
+# Loss for discrimination between real and fake images.
+criterionD = nn.BCELoss()
+# Loss for discrete latent code.
+criterionQ_dis = nn.CrossEntropyLoss()
+# Loss for continuous latent code.
+criterionQ_con = NormalNLLLoss()
+
+# Adam optimiser is used.
+optimD = optim.Adam([{'params': discriminator.parameters()}, {'params': netD.parameters()}], lr=params['learning_rate'], betas=(params['beta1'], params['beta2']))
+optimG = optim.Adam([{'params': netG.parameters()}, {'params': netQ.parameters()}], lr=params['learning_rate'], betas=(params['beta1'], params['beta2']))
+
+# Fixed Noise
+# z = torch.randn(100, params['num_z'], 1, 1, device=device)
+sample_batch = next(iter(dataloader))
+fixed_noise = sample_batch['CWN'][:2]
+fixed_noise = fixed_noise.to(device,dtype=torch.float32)
+
+real_label = 1
+fake_label = 0
+
+# List variables to store results pf training.
+img_list = []
+G_losses = []
+D_losses = []
+
+print("-"*25)
+print("Starting Training Loop...\n")
+print('Epochs: %d\nDataset: {}\nBatch Size: %d\nLength of Data Loader: %d'.format(params['dataset']) % (params['num_epochs'], params['batch_size'], len(dataloader)))
+print("-"*25)
+
+start_time = time.time()
+iters = 0
+
+for epoch in range(params['num_epochs']):
+    epoch_start_time = time.time()
+
+    for i, data_batch in enumerate(dataloader, 0):
+        data = data_batch['reflectance']
+        # Get batch size
+        b_size = data.size(0)
+        # Transfer data tensor to GPU/CPU (device)
+        real_data = data.to(device,dtype=torch.float32)
+
+        # Updating discriminator and DHead
+        optimD.zero_grad()
+        # Real data
+        label = torch.full((b_size, ), real_label, device=device).to(torch.float32)
+        output1 = discriminator(real_data)
+        probs_real = netD(output1).view(-1)
+        loss_real = criterionD(probs_real, label)
+        # Calculate gradients.
+        loss_real.backward()
+
+        # Fake data
+        label.fill_(fake_label)
+        # noise, idx = noise_sample(params['num_dis_c'], params['dis_c_dim'], params['num_con_c'], params['num_z'], b_size, device)
+        noise, idx = data_batch['CWN'], data_batch['idxx']
+        noise = noise.to(device,dtype=torch.float32)
+
+        fake_data = netG(noise)
+        output2 = discriminator(fake_data.detach())
+        probs_fake = netD(output2).view(-1)
+        loss_fake = criterionD(probs_fake, label)
+        # Calculate gradients.
+        loss_fake.backward()
+
+        # Net Loss for the discriminator
+        D_loss = loss_real + loss_fake
+        # Update parameters
+        optimD.step()
+
+        # Updating Generator and QHead
+        optimG.zero_grad()
+
+        # Fake data treated as real.
+        output = discriminator(fake_data)
+        label.fill_(real_label)
+        probs_fake = netD(output).view(-1)
+        gen_loss = criterionD(probs_fake, label)
+
+        q_logits, q_mu, q_var = netQ(output)
+        target = idx.to(device, dtype=torch.float32)
+
+        # print(q_logits.shape)
+        # print(target.shape)
+        # Calculating loss for discrete latent code.
+        dis_loss1 = criterionQ_dis(q_logits[:,:4],target[:,:4].softmax(dim=1))
+        dis_loss2 = criterionQ_dis(q_logits[:,4:],target[:,4:].softmax(dim=1))
+        dis_loss = dis_loss1+dis_loss2
+        # dis_loss = 0
+        # for j in range(params['num_dis_c']):
+        #     dis_loss += criterionQ_dis(q_logits[:, j], target[:,j])
+
+        # # Calculating loss for continuous latent code.
+        con_loss = 0
+        # if (params['num_con_c'] != 0):
+        #     con_loss = criterionQ_con(noise[:, params['num_z']+ params['num_dis_c']*params['dis_c_dim'] : ].view(-1, params['num_con_c']), q_mu, q_var)*0.1
+
+        # Net loss for generator.
+        G_loss = gen_loss + dis_loss + con_loss
+        # Calculate gradients.
+        G_loss.backward()
+        # Update parameters.
+        optimG.step()
+
+        # Check progress of training.
+        if i != 0 and i%100 == 0:
+            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f'
+                  % (epoch+1, params['num_epochs'], i, len(dataloader), 
+                    D_loss.item(), G_loss.item()))
+
+        # Save the losses for plotting.
+        G_losses.append(G_loss.item())
+        D_losses.append(D_loss.item())
+
+        iters += 1
+
+    epoch_time = time.time() - epoch_start_time
+    print("Time taken for Epoch %d: %.2fs" %(epoch + 1, epoch_time))
+    # Generate image after each epoch to check performance of the generator. Used for creating animated gif later.
+    with torch.no_grad():
+        gen_data = netG(fixed_noise).detach().cpu()
+    img_list.append(vutils.make_grid(gen_data, nrow=3, padding=2, normalize=True))
+
+    # Generate image to check performance of generator.
+    if((epoch+1) == 1 or (epoch+1) == params['num_epochs']/2):
+        with torch.no_grad():
+            gen_data = netG(fixed_noise).detach().cpu()
+        plt.figure(figsize=(10, 10))
+        plt.axis("off")
+        # plt.imshow(np.transpose(vutils.make_grid(gen_data, nrow=10, padding=2, normalize=True), (1,2,0)))
+        fname = "Epoch_%d {}".format(params['dataset']) %(epoch+1)
+        plot_cot2(gen_data[0,0],"Radiance at 0.66um",fname,False,[0,2])
+        # plt.savefig("Epoch_%d {}".format(params['dataset']) %(epoch+1))
+        plt.close('all')
+
+    # Save network weights.
+    if (epoch+1) % params['save_epoch'] == 0:
+        torch.save({
+            'netG' : netG.state_dict(),
+            'discriminator' : discriminator.state_dict(),
+            'netD' : netD.state_dict(),
+            'netQ' : netQ.state_dict(),
+            'optimD' : optimD.state_dict(),
+            'optimG' : optimG.state_dict(),
+            'params' : params
+            }, 'checkpoint/model_epoch_%d_{}'.format(params['dataset']) %(epoch+1))
+
+training_time = time.time() - start_time
+print("-"*50)
+print('Training finished!\nTotal Time for Training: %.2fm' %(training_time / 60))
+print("-"*50)
+
+# Generate image to check performance of trained generator.
+with torch.no_grad():
+    gen_data = netG(fixed_noise).detach().cpu()
+plt.figure(figsize=(10, 10))
+# plt.axis("off")
+# plt.imshow(np.transpose(vutils.make_grid(gen_data, nrow=10, padding=2, normalize=True), (1,2,0)))
+# plt.savefig("Epoch_%d_{}".format(params['dataset']) %(params['num_epochs']))
+fname = "Epoch_%d_{}".format(params['dataset']) %(params['num_epochs'])
+plot_cot2(gen_data[0,0],"Radiance at 0.66um",fname,False,[0,2])
+
+
+# Save network weights.
+torch.save({
+    'netG' : netG.state_dict(),
+    'discriminator' : discriminator.state_dict(),
+    'netD' : netD.state_dict(),
+    'netQ' : netQ.state_dict(),
+    'optimD' : optimD.state_dict(),
+    'optimG' : optimG.state_dict(),
+    'params' : params
+    }, 'checkpoint/model_final_{}'.format(params['dataset']))
+
+
+# Plot the training losses.
+plt.figure(figsize=(10,5))
+plt.title("Generator and Discriminator Loss During Training")
+plt.plot(G_losses,label="G")
+plt.plot(D_losses,label="D")
+plt.xlabel("iterations")
+plt.ylabel("Loss")
+plt.legend()
+plt.savefig("Loss Curve {}".format(params['dataset']))
+
+# Animation showing the improvements of the generator.
+fig = plt.figure(figsize=(10,10))
+plt.axis("off")
+ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
+anim = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
+anim.save('infoGAN_{}.gif'.format(params['dataset']), dpi=80, writer='imagemagick')
+plt.show()
